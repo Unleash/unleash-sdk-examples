f39ca23967714d3afdad18eb8a5db3053f171b49
Gard Rimestad <gard@getunleash.ai>
2023-12-07 15:50:21 +0100

    yes

--- ruby/app.rb
+++ ruby/app.rb
@@ -3,6 +3,8 @@ require 'unleash'
 UNLEASH = Unleash::Client.new(
     url: 'http://localhost:4242/api/',
     app_name: 'unleash-sdk-ruby-example',
-    custom_http_headers: {'Authorization': '*:development.cfaeef37b291b5190eb815b228d8a511dad8797fd446f5ab7bfe41e2'}
+    custom_http_headers: {'Authorization': 'apiKey'}
     )
+pp UNLEASH.is_enabled?('test-flag')
+sleep 1
 pp UNLEASH.is_enabled?('test-flag')
