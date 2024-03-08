dabb1f6b37f4ec2564d469e81fe27fd657fbf3b6
Thomas Heartman <thomas@getunleash.io>
2024-03-08 12:44:57 +0100

    chore: add devenv config

---
+++ devenv.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/languages/
+  languages.java.enable = true;
+  languages.javascript.enable = true;
+  languages.dotnet.enable = true;
+  languages.php.enable = true;
+  languages.ruby.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
