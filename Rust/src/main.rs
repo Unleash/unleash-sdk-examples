37122b06f6a3c8f36acc7f76b1cdad8470908176
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2024-09-06 15:08:06 +0200

    update rust example

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -10,20 +10,18 @@ use unleash_api_client::Client;
 
 #[derive(Debug, Deserialize, Serialize, Enum, Clone)]
 enum Flags {
-    #[serde(rename = "demoApp.step1")] // TODO: Flag name
-    RustCodesandbox,
+    #[serde(rename = "flag-rust")] // TODO: Flag name
+    TestFlag,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     dotenv().expect(".env file not found");
-
-    // Read environment variables
     let api_url = env::var("UNLEASH_API_URL").expect("UNLEASH_API_URL not found");
     let token = env::var("UNLEASH_API_TOKEN").expect("UNLEASH_API_TOKEN not found");
 
     let client: Client<Flags, reqwest::Client> = ClientBuilder::default()
-        .interval(500)
+        .interval(5000) // Polling & metrics interval - default 15000 (ms)
         .into_client(
             &api_url,
             "codesandbox", // App name
@@ -33,13 +31,12 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     client.register().await?;
 
     let (_, _) = tokio::join!(client.poll_for_updates(), async {
-        // Ensure we have features for this demo.
-        sleep(Duration::from_millis(500)).await;
-
-        let is_webp = client.is_enabled(Flags::RustCodesandbox, None, false);
-        println!("Is flag enabled {}", is_webp);
-
         sleep(Duration::from_millis(1000)).await;
+
+        let is_enabled = client.is_enabled(Flags::TestFlag, None, true);
+        println!("\nIs flag enabled: {}\n", is_enabled);
+
+        sleep(Duration::from_millis(5000)).await;
 
         client.stop_poll().await;
         Ok::<(), Box<dyn Error + Send + Sync>>(())
