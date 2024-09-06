a5a0862d73014e72443b3684d2d360e7776c79d9
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2024-09-06 15:41:50 +0200

    react readme

---
+++ React/README.md
@@ -0,0 +1,5 @@
+# Unleash React SDK
+
+Here you can easily get started with [Unleash SDK](https://github.com/Unleash/proxy-client-react) for [React](https://react.dev/) and experiment.
+
+Check out [src/App.tsx](./src/App.tsx) example and [src/index.tsx](./src/index.tsx) with SDK configuration.
--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,4 +1,4 @@
-import { useFlag } from "@unleash/proxy-client-react";
+import { useFlag, useVariant } from "@unleash/proxy-client-react";
 import "./styles.css";
 
 export default function App() {
@@ -7,6 +7,11 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <p>
+        Here you can easily get started with{" "}
+        <a href="https://github.com/Unleash/proxy-client-react">Unleash SDK</a>{" "}
+        for <a href="https://react.dev/">React</a> and experiment.
+      </p>
       <p>
         Flag is <strong>{isEnabled ? "enabled" : "disabled"}</strong>
       </p>
