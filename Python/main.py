d4685c7286cf4fb28db1a8ea3e9f7c7438d620a9
Thomas Heartman <thomas@getunleash.io>
2024-09-06 13:47:54 +0200

    python: add unleash client dependency and check

--- Python/main.py
+++ Python/main.py
@@ -1,1 +1,18 @@
-print("Hello CodeSandbox!")
+from UnleashClient import UnleashClient
+import os
+
+api_url = os.environ.get('UNLEASH_API_URL')
+api_token = os.environ.get('UNLEASH_API_TOKEN')
+flag = "demoApp.step1"
+
+client = UnleashClient(
+    url=api_url,
+    app_name="unleash-python-example",
+    custom_headers={'Authorization': api_token})
+
+client.initialize_client()
+
+if client.is_enabled(flag):
+    print("Feature flag '{}' is enabled".format(flag))
+else:
+    print("Feature flag '{}' is disabled".format(flag))
