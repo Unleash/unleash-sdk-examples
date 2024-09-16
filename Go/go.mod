c893218ba405ce9f545af0310073df95e5a7e7b7
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2024-09-16 13:14:07 +0200

    unleash go example with metrics

---
+++ Go/go.mod
@@ -0,0 +1,17 @@
+module example/unleash_test
+
+go 1.23.1
+
+require (
+	github.com/Unleash/unleash-client-go/v4 v4.1.3
+	github.com/joho/godotenv v1.5.1
+)
+
+require (
+	github.com/Masterminds/semver/v3 v3.1.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.1.1 // indirect
+	github.com/stretchr/testify v1.2.2 // indirect
+	github.com/twmb/murmur3 v1.1.8 // indirect
+)
--- Go/main.go
+++ Go/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/Unleash/unleash-client-go/v4"
+	"github.com/joho/godotenv"
+)
+
+const FlagName = "flag-go"
 
 func main() {
-	fmt.Println("Hello, Go in CodeSandbox!")
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	unleash.Initialize(
+		unleash.WithListener(&unleash.DebugListener{}),
+		unleash.WithAppName("codesandbox-go"),
+		unleash.WithUrl(os.Getenv("UNLEASH_API_URL")),
+		unleash.WithCustomHeaders(http.Header{"Authorization": {os.Getenv("UNLEASH_API_TOKEN")}}),
+		unleash.WithRefreshInterval(15*time.Second),
+		unleash.WithMetricsInterval(5*time.Second),
+	)
+
+	timer := time.NewTimer(1 * time.Second)
+
+	for {
+		<-timer.C
+		fmt.Printf("\033[32m'%s' enabled? %v\033[0m\n", FlagName, unleash.IsEnabled(FlagName))
+		timer.Reset(3 * time.Second)
+	}
 }
