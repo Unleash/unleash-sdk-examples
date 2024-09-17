77c4720d0aa3a87ab20fe84dc86e1b2d35506099
kwasniew <kwasniewski.mateusz@gmail.com>
2024-09-17 11:29:53 +0200

    feat: swift

--- Swift/Package.swift
+++ Swift/Package.swift
@@ -5,8 +5,8 @@ import PackageDescription
 let package = Package(
     name: "UnleashExample",
     platforms: [
-            .macOS(.v10_15),
-            .iOS(.v12),
+            .macOS("10.15")
+            .iOS("12.0"),
     ],
     dependencies: [
         .package(url: "https://github.com/Unleash/unleash-proxy-client-swift.git", branch: "main")
