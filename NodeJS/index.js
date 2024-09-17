15426c8a0356a6b450cbfffb94a3664be841580a
Thomas Heartman <thomas@getunleash.io>
2024-09-17 15:05:44 +0200

    nodejs: simplify index.js

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -1,13 +1,9 @@
 const { initialize, isEnabled, getVariant } = require('unleash-client');
 require('dotenv').config()
 
-console.log(`Unleash API URL is ${process.env.UNLEASH_API_URL}`);
-console.log(`Unleash API token is ${process.env.UNLEASH_API_TOKEN}`);
-
 const apiUrl = process.env.UNLEASH_API_URL
 const apiToken = process.env.UNLEASH_API_TOKEN
 const flag = 'example-flag'
-
 
 // Initialize the Unleash client
 const instance = initialize({
@@ -20,22 +16,11 @@ const instance = initialize({
 });
 
 instance.on('ready', () => {
-  const flags = ['child-1', 'child-2', 'child-3', 'child-4', 'child-variant-dependency', 'is-enabled-child', 'get-variant-child'];
-
-  flags.forEach((flag) => {
-    if (isEnabled(flag)) {
-      console.log(`${flag} flag is enabled`);
+  if (unleash.isEnabled(flag)) {
+      console.log(`Flag '${flag}' is enabled`);
     } else {
-      console.log(`${flag} flag is disabled`);
+      console.log(`Flag '${flag}' is disabled`);
     }
-  });
-
-  const variantChecks = ['child-variant-dependency', 'get-variant-child'];
-
-  variantChecks.forEach((flag) => {
-    const variant = getVariant(flag);
-    console.log(`Got variant ${JSON.stringify(variant)} for flag ${flag}`);
-  });
 
   // Sending metrics (imitating the Java sleep)
   setTimeout(() => {
