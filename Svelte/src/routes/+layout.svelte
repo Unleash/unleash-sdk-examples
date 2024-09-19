7c42dcb8f2cd2b790d1aacada1fd4b4a06de588e
Tymoteusz Czech <2625371+Tymek@users.noreply.github.com>
2024-09-19 12:02:26 +0200

    svelte basic template

---
+++ Svelte/src/routes/+layout.svelte
@@ -0,0 +1,59 @@
+<script>
+	import './styles.css';
+	import { FlagProvider } from '@unleash/proxy-client-svelte';
+
+	const config = {
+		url: 'https://app.unleash-hosted.com/demo/api/frontend',
+		clientKey:
+			'proxy-client-svelte:development.f7d5c2f3633769afea4eadecd6bf46194dccec9c75170930c76ab4f0',
+		refreshInterval: 2,
+		appName: 'proxy-client-svelte'
+	};
+</script>
+
+<div class="app">
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+	</footer>
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 64rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 12px;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
+</style>
---
+++ Svelte/src/routes/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+
+	// import { useFlag, useFlagsStatus, useVariant } from '@unleash/proxy-client-svelte';
+
+	// const enabled = useFlag('svelte-test-feature');
+	// const variant = useVariant('svelte-test-feature');
+	// const { flagsReady } = useFlagsStatus();
+
+	// $: if ($flagsReady) console.table({ $enabled, $variant });
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>
+		to your new<br />SvelteKit app
+	</h1>
+
+	<h2>
+		try editing <strong>src/routes/+page.svelte</strong>
+	</h2>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+</style>
