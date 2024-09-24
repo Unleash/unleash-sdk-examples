c39a50507f0bb48a6def8178ff463bda48a3c83c
sjaanus <sellinjaanus@gmail.com>
2024-09-24 16:13:28 +0300

    Test

--- Java/src/main/java/Main.java
+++ Java/src/main/java/Main.java
@@ -1,7 +1,28 @@
 package main.java;
 
+import io.getunleash.DefaultUnleash;
+import io.getunleash.Unleash;
+import io.getunleash.util.UnleashConfig;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws InterruptedException {
+        var flag = "example-flag";
+        var url = System.getenv("UNLEASH_API_URL");
+        var token = System.getenv("UNLEASH_API_TOKEN");
+
+        UnleashConfig config = UnleashConfig.builder()
+                .appName("codesandbox-java")
+                .unleashAPI(url)
+                .apiKey(token)
+                .sendMetricsInterval(5)
+                .build();
+
+        Unleash unleash = new DefaultUnleash(config);
+
+        while (true) {
+            boolean featureEnabled = unleash.isEnabled(flag);
+            System.out.println("Feature enabled: " + featureEnabled);
+            Thread.sleep(1000);
+        }
     }
 }
