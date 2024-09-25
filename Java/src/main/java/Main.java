a3f62cd1c7c51681f1441053a65d4fe26f7c52e3
sjaanus <sellinjaanus@gmail.com>
2024-09-25 11:10:52 +0300

    Fix

--- Java/src/main/java/Main.java
+++ Java/src/main/java/Main.java
@@ -7,8 +7,8 @@ import io.getunleash.util.UnleashConfig;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         var flag = "example-flag";
-        var url = System.getenv("UNLEASH_API_URL");
-        var token = System.getenv("UNLEASH_API_TOKEN");
+        var url = "https://app.unleash-hosted.com/demo/api";
+        var token = "demo-app:dev.95ae66ab673bf467facb68b2487904f4891064d26b47e89ca498063d";
 
         UnleashConfig config = UnleashConfig.builder()
                 .appName("codesandbox-java")
