41658964910c2ecc8cdca5315947f25103842de4
Kris (K) Kula <kris.kula@getunleash.io>
2026-05-28 15:45:06 +0200

    better svelte CI checks

---
+++ Svelte/eslint.config.js
@@ -0,0 +1,24 @@
+import js from '@eslint/js';
+import ts from 'typescript-eslint';
+import svelte from 'eslint-plugin-svelte';
+import prettier from 'eslint-config-prettier';
+import globals from 'globals';
+
+export default ts.config(
+	js.configs.recommended,
+	...ts.configs.recommended,
+	...svelte.configs['flat/recommended'],
+	prettier,
+	{
+		languageOptions: {
+			globals: { ...globals.browser, ...globals.node }
+		}
+	},
+	{
+		files: ['**/*.svelte'],
+		languageOptions: {
+			parserOptions: { parser: ts.parser }
+		}
+	},
+	{ ignores: ['*.cjs', '.svelte-kit/**'] }
+);
