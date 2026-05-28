9d227ddda822d97528fa28d20fe183a89db0a50f
Kris (K) Kula <kris.kula@getunleash.io>
2026-05-28 16:54:14 +0200

    Prettier

--- Next.js/app/page.tsx
+++ Next.js/app/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import styles from "./page.module.css";
 import { FlagProvider } from "@unleash/nextjs";
--- Next.js/eslint.config.mjs
+++ Next.js/eslint.config.mjs
@@ -1,6 +1,7 @@
 import coreWebVitals from "eslint-config-next/core-web-vitals";
 import nextTypescript from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier";
 
-const config = [...coreWebVitals, ...nextTypescript];
+const config = [...coreWebVitals, ...nextTypescript, prettier];
 
 export default config;
