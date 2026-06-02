fef25c8fbc20868aa7933c8b21a02048be057d08
Kris (K) Kula <kris.kula@getunleash.io>
2026-06-02 14:22:54 +0200

    newest ruby sdk and smoke test

--- Ruby/README.md
+++ Ruby/README.md
@@ -3,3 +3,10 @@
 [![Edit in CodeSandbox](https://codesandbox.io/static/img/play-codesandbox.svg)](https://codesandbox.io/s/github/Unleash/unleash-sdk-examples/tree/main/Ruby)
 
 Quickly get started with [Unleash SDK](https://github.com/Unleash/unleash-ruby-sdk) using this starter! Check out [example.rb](./example.rb) for the code. Set your flag name in line `:29`. Put you instance URL and API client token in `.env`.
+
+```sh
+bundle install
+cp -n .env.example .env
+
+bundle exec ruby example.rb
+```
--- Ruby/example.rb
+++ Ruby/example.rb
@@ -4,25 +4,28 @@ require 'unleash'
 require 'unleash/context'
 require 'dotenv/load'
 
+abort "Missing UNLEASH_API_URL in .env" unless ENV["UNLEASH_API_URL"]
+abort "Missing UNLEASH_API_TOKEN in .env" unless ENV["UNLEASH_API_TOKEN"]
+
 @unleash = Unleash::Client.new(
-  url: ENV["UNLEASH_API_URL"],
-  custom_http_headers: { 'Authorization': ENV["UNLEASH_API_TOKEN"] },
-  app_name: 'codesandbox-ruby',
-  instance_id: 'codesandbox-ruby',
-  refresh_interval: 3,
-  metrics_interval: 3,
-  retry_limit: 2
+    url: ENV["UNLEASH_API_URL"],
+    custom_http_headers: { 'Authorization': ENV["UNLEASH_API_TOKEN"] },
+    app_name: 'codesandbox-ruby',
+    instance_id: 'codesandbox-ruby',
+    refresh_interval: 3,
+    metrics_interval: 3,
+    retry_limit: 2
 )
 
 # or:
 
 # Unleash.configure do |config|
-#   config.url = ENV["UNLEASH_API_URL"]
-#   config.custom_http_headers = { 'Authorization': ENV["UNLEASH_API_TOKEN"] }
-#   config.app_name = 'simple-test'
-#   config.refresh_interval = 2
-#   config.metrics_interval = 2
-#   config.retry_limit = 2
+#     config.url = ENV["UNLEASH_API_URL"]
+#     config.custom_http_headers = { 'Authorization': ENV["UNLEASH_API_TOKEN"] }
+#     config.app_name = 'simple-test'
+#     config.refresh_interval = 2
+#     config.metrics_interval = 2
+#     config.retry_limit = 2
 # end
 # @unleash = Unleash::Client.new
 
@@ -32,12 +35,12 @@ unleash_context.user_id = 123
 
 sleep 1
 30.times do
-  if @unleash.is_enabled?(feature_flag, unleash_context)
-    puts "#{feature_flag} is enabled"
-  else
-    puts "#{feature_flag} is not enabled"
-  end
-  sleep 3
+    if @unleash.is_enabled?(feature_flag, unleash_context)
+        puts "#{feature_flag} is enabled"
+    else
+        puts "#{feature_flag} is not enabled"
+    end
+    sleep 3
 end
 
 puts "shutting down"
