f0e17d6fda6708a0a5ca16e88efdfe8fbdabea6d
Kris (K) Kula <kris.kula@getunleash.io>
2026-06-03 14:10:16 +0200

    lint

---
+++ JavaScript/eslint.config.mjs
@@ -0,0 +1,12 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+	js.configs.recommended,
+	{
+		languageOptions: {
+			globals: { ...globals.browser }
+		}
+	},
+	{ ignores: ['dist/**', '.parcel-cache/**'] }
+];
