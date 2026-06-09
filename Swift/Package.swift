530f25037a75c94deea0c0986cb608e131ee39f6
Kris (K) Kula <kris.kula@getunleash.io>
2026-06-09 15:50:48 +0200

    use static version

--- Swift/Package.swift
+++ Swift/Package.swift
@@ -5,19 +5,19 @@ import PackageDescription
 let package = Package(
     name: "UnleashExample",
     platforms: [
-            .macOS(.v13),
-            .iOS(.v13),
+        .macOS(.v13),
+        .iOS(.v13),
     ],
     dependencies: [
-        .package(url: "https://github.com/Unleash/unleash-ios-sdk.git", branch: "main"),
-        .package(url: "https://github.com/thebarndog/swift-dotenv.git", from: "2.0.0")
+        .package(url: "https://github.com/Unleash/unleash-ios-sdk.git", from: "2.4.0"),
+        .package(url: "https://github.com/thebarndog/swift-dotenv.git", from: "2.0.0"),
     ],
     targets: [
         .executableTarget(
             name: "UnleashExample",
             dependencies: [
                 .product(name: "UnleashProxyClientSwift", package: "unleash-ios-sdk"),
-                .product(name: "SwiftDotenv", package: "swift-dotenv")
+                .product(name: "SwiftDotenv", package: "swift-dotenv"),
             ]
         )
     ]
