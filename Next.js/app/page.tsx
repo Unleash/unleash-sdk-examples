fd0e57ca2ce07e614836fb6ef8ad5c76b3d549a5
Kris (K) Kula <kris.kula@getunleash.io>
2026-06-10 12:59:42 +0200

    use frontend api url

--- Next.js/app/page.tsx
+++ Next.js/app/page.tsx
@@ -8,7 +8,13 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <FlagProvider>
+        <FlagProvider
+          config={{
+            url: process.env.NEXT_PUBLIC_UNLEASH_FRONTEND_API_URL ?? "",
+            clientKey: process.env.NEXT_PUBLIC_UNLEASH_FRONTEND_API_TOKEN ?? "",
+            appName: process.env.NEXT_PUBLIC_UNLEASH_APP_NAME ?? "",
+          }}
+        >
           <ClientComponent />
         </FlagProvider>
       </main>
