218e61bed7014fd2644e92b9bdca86c29e455e01
Krzysztof (Kris) Kula <kris.kula@getunleash.io>
2026-06-10 11:17:11 +0200

    chore: iOS/swift sdk update (#116)
    * chore: swift sdk example improvements
    
    * use static version
    
    * 6.3
    
    * no version on CI
    
    * make sure we print

--- Swift/Package.swift
+++ Swift/Package.swift
@@ -1,21 +1,23 @@
-// swift-tools-version:5.6
+// swift-tools-version:6.0
 
 import PackageDescription
 
 let package = Package(
     name: "UnleashExample",
     platforms: [
-            .macOS(.v10_15),
-            .iOS(.v12),
+        .macOS(.v13),
+        .iOS(.v13),
     ],
     dependencies: [
-        .package(url: "https://github.com/Unleash/unleash-ios-sdk.git", branch: "main")
+        .package(url: "https://github.com/Unleash/unleash-ios-sdk.git", from: "2.4.0"),
+        .package(url: "https://github.com/thebarndog/swift-dotenv.git", from: "2.0.0"),
     ],
     targets: [
         .executableTarget(
             name: "UnleashExample",
             dependencies: [
-                .product(name: "UnleashProxyClientSwift", package: "unleash-ios-sdk")
+                .product(name: "UnleashProxyClientSwift", package: "unleash-ios-sdk"),
+                .product(name: "SwiftDotenv", package: "swift-dotenv"),
             ]
         )
     ]
--- Swift/README.md
+++ Swift/README.md
@@ -4,9 +4,16 @@ Codesandbox not supported yet.
 
 Here you can easily get started with [Unleash SDK](https://github.com/Unleash/unleash-ios-sdk) for [Swift](https://www.swift.org/) and experiment.
 
-Check out [Sources/UnleashExample/main.swift](./Sources/UnleashExample/main.swift) example.
+Check out [Sources/UnleashExample/main.swift](./Sources/UnleashExample/main.swift) for the code. Set your flag name and put your instance URL and client key in `.env`.
+
+## Setup
+
+```sh
+cp -n .env.example .env
+```
 
 ## Run locally
 
-* `swift build`
-* `swift run`
+```sh
+swift run
+```
