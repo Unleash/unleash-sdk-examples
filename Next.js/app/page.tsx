4fee9a093bbe3c6c440c1117b04b641c171e804f
Kris (K) Kula <kris.kula@getunleash.io>
2026-06-10 16:05:03 +0200

    config without defaults

--- Next.js/app/page.tsx
+++ Next.js/app/page.tsx
@@ -4,17 +4,17 @@ import { FlagProvider } from "@unleash/nextjs/client";
 import ClientComponent from "../components/ClientComponent";
 import styles from "./page.module.css";
 
+const config = {
+  url: process.env.NEXT_PUBLIC_UNLEASH_FRONTEND_API_URL,
+  clientKey: process.env.NEXT_PUBLIC_UNLEASH_FRONTEND_API_TOKEN,
+  appName: process.env.NEXT_PUBLIC_UNLEASH_APP_NAME,
+};
+
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <FlagProvider
-          config={{
-            url: process.env.NEXT_PUBLIC_UNLEASH_FRONTEND_API_URL ?? "",
-            clientKey: process.env.NEXT_PUBLIC_UNLEASH_FRONTEND_API_TOKEN ?? "",
-            appName: process.env.NEXT_PUBLIC_UNLEASH_APP_NAME ?? "",
-          }}
-        >
+        <FlagProvider config={config}>
           <ClientComponent />
         </FlagProvider>
       </main>
